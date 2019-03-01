477c9c466e86e258e41ef1bf0f26ceaa0b93c4c3
khaibin <bkchow@yes.my>
2019-03-01 13:15:32 +0800

    ai bot implementation

--- server.py
+++ server.py
@@ -1,7 +1,18 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-import database as db
 import json
+import random
+import string
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import database as db
+import warnings
+warnings.filterwarnings("ignore")
+
+#nltk.download('punkt')
+#nltk.download('wordnet')
+#nltk.download('stopwords')
 
 
 def handle_connection(connection: socket):
@@ -19,7 +30,7 @@ def handle_connection(connection: socket):
         connection.close()
 
 
-def action_flow(connection: socket, client_id, screen_id):
+def action_flow(connection: socket, client_id, screen_id, description):
     response = {}
 
     while True:
@@ -38,7 +49,7 @@ def action_flow(connection: socket, client_id, screen_id):
             data = screen['options']['value'][screen['options']['text'].index(data)]
 
             if data == 'bot':
-                action_bot(connection)
+                action_bot(connection, description)
         else:
             screen_id = screen['next_screen_id']
 
@@ -54,14 +65,65 @@ def action_flow(connection: socket, client_id, screen_id):
         db.put_response(client_id, to_json(response))
 
 
-def action_bot(connection: socket):
-    send(connection, {'text': 'Hello, I am AI bot. I am here to answer your questions', 'options': []})
+def action_bot(connection: socket, description):
+    def greeting(sentence):
+        for word in sentence.split():
+            if word.lower() in GREETING_INPUTS:
+                return random.choice(GREETING_RESPONSES)
 
-    while True:
-        data = receive(connection)
-        if data == 'thanks':
-            break
-        send(connection, {'text': 'I am not sure I understand your questions', 'options': []})
+    def response(user_response):
+        robo_response = ''
+        sent_tokens.append(user_response)
+        TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+        tfidf = TfidfVec.fit_transform(sent_tokens)
+        vals = cosine_similarity(tfidf[-1], tfidf)
+        idx = vals.argsort()[0][-2]
+        flat = vals.flatten()
+        flat.sort()
+        req_tfidf = flat[-2]
+        if req_tfidf == 0:
+            robo_response = robo_response + "I am sorry! I don't understand you"
+            return robo_response
+        else:
+            robo_response = robo_response + sent_tokens[idx]
+            return robo_response
+
+    def LemTokens(tokens):
+        return [lemmer.lemmatize(token) for token in tokens]
+
+    def LemNormalize(text):
+        return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+    description = description.lower()
+
+    sent_tokens = nltk.sent_tokenize(description)
+    lemmer = nltk.stem.WordNetLemmatizer()
+
+    remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+    GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey",)
+    GREETING_RESPONSES = ["hi", "hey", "hi there", "hello", "I am glad! You are talking to me"]
+
+    flag = True
+    send(connection, {'text': 'Hi, I am AI Bot and I am here to answer your questions!', 'options': []})
+    send(connection, {'text': 'If you want to exit, type Bye!', 'options': []})
+
+    while flag:
+        user_response = input()
+        user_response = user_response.lower()
+        if user_response != 'bye':
+            if user_response == 'thanks' or user_response == 'thank you':
+                flag = False
+                send(connection, {'text': 'You are welcome..', 'options': []})
+            else:
+                if greeting(user_response) is not None:
+                    send(connection, {'text': greeting(user_response), 'options': []})
+                else:
+                    send(connection, {'text': response(user_response), 'options': []})
+                    sent_tokens.remove(user_response)
+        else:
+            flag = False
+            send(connection, {'text': 'Bye', 'options': []})
 
 
 def accept_connections():
