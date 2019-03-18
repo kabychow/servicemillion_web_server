e78788a4fbcb523d83fe64f8479cdb15df498002
khaibin <bkchow@yes.my>
2019-03-18 16:42:52 +0800

    Refactor - now it supports real time refresh of database

--- database.py
+++ database.py
@@ -1,40 +1,49 @@
 import mysql.connector as mariadb
-
-db = mariadb.connect(user='root', password='root', database='superceed')
-
-
-def get_client(client_id):
-    cursor = db.cursor(dictionary=True)
-    cursor.execute('SELECT id, greetings, description FROM client WHERE id=%s', (client_id,))
-    client = cursor.fetchone()
-    client['screens'] = {'id': [], 'title': []}
-    cursor.execute('SELECT title, screen_id FROM client_screen WHERE client_id=%s', (client_id,))
-    for screen in cursor.fetchall():
-        client['screens']['id'].append(screen['screen_id'])
-        client['screens']['title'].append(screen['title'])
-    cursor.close()
-    return client
+import config
 
 
-def get_screen(screen_id):
-    cursor = db.cursor(dictionary=True)
-    cursor.execute('SELECT title, text, label, is_array, next_screen_id FROM screen WHERE id = %s', (screen_id,))
-    screen = cursor.fetchone()
-    screen['options'] = {'text': [], 'value': [], 'next_screen_id': []}
-    cursor.execute('SELECT text, value, next_screen_id FROM screen_option WHERE screen_id=%s', (screen_id,))
-    for option in cursor.fetchall():
-        screen['options']['text'].append(option['text'])
-        screen['options']['value'].append(option['value'])
-        screen['options']['next_screen_id'].append(option['next_screen_id'])
-    screen['is_last'] = screen['next_screen_id'] is None and len(screen['options']['next_screen_id']) == 0
-    cursor.close()
-    return screen
+class Database:
+    def __init__(self):
+        self.db = mariadb.connect(user=config.DB_USER, password=config.DB_PASSWORD, database='superceed')
 
+    def get_client(self, client_id):
+        cursor = self.db.cursor(dictionary=True)
+        cursor.execute('SELECT id, greetings, description FROM client WHERE id=%s', (client_id,))
+        client = cursor.fetchone()
+        client['screens'] = {'id': [], 'title': []}
+        cursor.execute('SELECT title, screen_id FROM client_screen WHERE client_id=%s', (client_id,))
+        for screen in cursor.fetchall():
+            client['screens']['id'].append(screen['screen_id'])
+            client['screens']['title'].append(screen['title'])
+        cursor.close()
+        return client
 
-def put_response(client_id, data):
-    cursor = db.cursor()
-    cursor.execute('INSERT INTO response(client_id, data) VALUES(%s, %s)', (client_id, data))
-    response_id = cursor.lastrowid
-    cursor.close()
-    db.commit()
-    return response_id
+    def get_screen(self, screen_id):
+        cursor = self.db.cursor(dictionary=True)
+        cursor.execute('SELECT title, text, label, is_array, next_screen_id FROM screen WHERE id=%s', (screen_id,))
+        screen = cursor.fetchone()
+        screen['options'] = {'text': [], 'value': [], 'next_screen_id': []}
+        cursor.execute('SELECT text, value, next_screen_id FROM screen_option WHERE screen_id=%s', (screen_id,))
+        for option in cursor.fetchall():
+            screen['options']['text'].append(option['text'])
+            screen['options']['value'].append(option['value'])
+            screen['options']['next_screen_id'].append(option['next_screen_id'])
+        cursor.close()
+        return screen
+
+    def put_response(self, client_id, data):
+        cursor = self.db.cursor()
+        cursor.execute('INSERT INTO response(client_id, data) VALUES(%s, %s)', (client_id, data))
+        response_id = cursor.lastrowid
+        cursor.close()
+        self.db.commit()
+        return response_id
+
+    def get_fcm_tokens(self,client_id):
+        fcm_tokens = []
+        cursor = self.db.cursor()
+        cursor.execute('SELECT fcm_token FROM client_user WHERE client_id=%s AND status=1', (client_id,))
+        for fcm_token, in cursor.fetchall():
+            fcm_tokens.append(fcm_token)
+        cursor.close()
+        return fcm_tokens
--- server.py
+++ server.py
@@ -1,199 +1,230 @@
 import asyncio
-import websockets
-from websockets.exceptions import ConnectionClosed
 import json
 import string
+import uuid
+import websockets
 import nltk
+import requests
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import database as db
+import config
+from database import Database
+
+queue = {}
 
 
-async def handle_connection(socket, _):
-    data = json.loads(await socket.recv())
-    if 'response_id' not in data:
-        client = db.get_client(data['client_id'])
-        while True:
-            await socket.send(to_json({
-                'text': client['greetings'],
-                'options': client['screens']['title']
-            }))
-            data = await socket.recv()
-            if data in client['screens']['title']:
-                index = client['screens']['title'].index(data)
-                await action_flow(socket, client, client['screens']['id'][index])
-    else:
-        try:
-            current = queue[data['client_id']][data['response_id']]
-            if current['socket'] is not None and current['_socket'] is None:
-                current['_socket'] = socket
-                while True:
-                    try:
-                        await current['socket'].send(to_json({'text': await socket.recv(), 'options': []}))
-                    except ConnectionClosed:
-                        await current['socket'].send(to_json({'text': 'Chat session ended.', 'options': ['Restart']}))
-                        break
-        except (IndexError, KeyError):
-            pass
-        finally:
-            socket.close()
+class Handler:
+    @staticmethod
+    async def handle(socket, _):
+        handler = Handler(socket)
+        await handler.init()
 
+    def __init__(self, socket):
+        self.db = Database()
+        self.socket = socket
+        self.client = None
+        self.reply_id = None
+        self.response = {}
 
-async def action_flow(socket, client, screen_id):
-    response = {}
-    while True:
-        screen = db.get_screen(screen_id)
-        await socket.send(to_json({
-            'text': screen['text'],
-            'options': screen['options']['text']
-        }))
-        data = await socket.recv()
-        if screen['is_last']:
-            break
-        if len(screen['options']['text']) > 0:
-            if data not in screen['options']['text']:
-                continue
-            index = screen['options']['text'].index(data)
-            screen_id = screen['options']['next_screen_id'][index]
-            data = screen['options']['value'][index]
-            if data == 'bot':
-                await action_bot(socket, client)
+    async def init(self):
+        data = json.loads(await self.socket.recv())
+        self.client = self.db.get_client(data['client_id'])
+        if 'reply_id' in data:
+            self.reply_id = data['reply_id']
+            await self.action_reply()
         else:
-            screen_id = screen['next_screen_id']
-        if screen['label'] != '':
-            if screen['is_array'] == 0:
-                response[screen['label']] = data
-            else:
-                if screen['label'] not in response:
-                    response[screen['label']] = []
-                response[screen['label']].append(data)
-    if len(response) > 0:
-        response_id = db.put_response(client['id'], to_json(response))
-        final = 'Tracking ID: #' + str(response_id) + '\n\n'
-        for key in response:
-            final += key + ': '
-            if isinstance(response[key], list):
-                for item in response[key]:
-                    final += item + ', '
-                final = final[:-2]
-            else:
-                final += response[key]
-            final += '\n'
-        await socket.send(to_json({
-            'text': final,
-            'options': []
-        }))
+            await self.action_flow()
 
-
-async def action_bot(socket, client):
-    def normalize(text):
-        lemma = nltk.stem.WordNetLemmatizer()
-        return [lemma.lemmatize(token) for token in
-                nltk.word_tokenize(text.translate(dict((ord(punct), None) for punct in string.punctuation)))]
-
-    sent_tokens = nltk.sent_tokenize(client['description'].lower())
-    await socket.send(to_json({
-        'text': 'Hi, I am AI Bot and I am here to answer your questions!',
-        'options': []
-    }))
-    while True:
-        data = str(await socket.recv()).lower()
-        if data in ['hi', 'hello', 'hey', 'yo']:
-            await socket.send(to_json({
-                'text': 'Hey',
-                'options': []
-            }))
-        elif data in ['ok', 'okay', 'oh', 'k', 'kk', 'no problem', 'can', 'i see', 'lol', 'haha', 'hahaha', 'ya', 'yeah']:
-            await socket.send(to_json({
-                'text': 'Yeah',
-                'options': []
-            }))
-        elif data in ['thanks', 'thank you']:
-            await socket.send(to_json({
-                'text': 'You are welcome',
-                'options': []
-            }))
-            break
-        elif data in ['bye']:
-            await socket.send(to_json({
-                'text': 'Goodbye!',
-                'options': []
-            }))
-            break
-        else:
-            sent_tokens.append(data)
-            vector = TfidfVectorizer(stop_words='english', tokenizer=normalize).fit_transform(sent_tokens)
-            values = cosine_similarity(vector[-1], vector)
-            idx = values.argsort()[0][-2]
-            flat = values.flatten()
-            flat.sort()
-            if flat[-2] == 0:
-                await socket.send(to_json({
-                    'text': 'I am not sure with that',
-                    'options': []
-                }))
-                await socket.send(to_json({
-                    'text': 'Do you want to contact customer support?',
-                    'options': ['Yes', 'No']
-                }))
-                if await socket.recv() == 'Yes':
-                    await action_chat(socket, client, data)
-                    break
-                else:
-                    await socket.send(to_json({
-                        'text': 'I am smarter when you ask me questions with simple keywords :)',
+    async def action_reply(self):
+        current = queue[self.client['id']][self.reply_id]
+        if current['socket'] is not None and current['_socket'] is None:
+            current['_socket'] = self.socket
+            while True:
+                try:
+                    await current['socket'].send(to_json({
+                        'text': await self.socket.recv(),
                         'options': []
                     }))
-            else:
-                await socket.send(to_json({
-                    'text': sent_tokens[idx],
+                except websockets.exceptions.ConnectionClosed:
+                    await current['socket'].send(to_json({
+                        'text': 'Chat session ended.',
+                        'options': ['Restart']
+                    }))
+                    break
+        self.socket.close()
+
+    async def action_flow(self):
+        while True:
+            await self.socket.send(to_json({
+                'text': self.client['greetings'],
+                'options': self.client['screens']['title']
+            }))
+            data = await self.socket.recv()
+            if data in self.client['screens']['title']:
+                screen_id = self.client['screens']['id'][self.client['screens']['title'].index(data)]
+                while True:
+                    screen = self.db.get_screen(screen_id)
+                    await self.socket.send(to_json({
+                        'text': screen['text'],
+                        'options': screen['options']['text']
+                    }))
+                    if len(screen['options']['text']) == 0 and screen['next_screen_id'] is None:
+                        break
+                    data = await self.socket.recv()
+                    if len(screen['options']['text']) > 0:
+                        if data not in screen['options']['text']:
+                            continue
+                        screen_id = screen['options']['next_screen_id'][screen['options']['text'].index(data)]
+                        data = screen['options']['value'][screen['options']['text'].index(data)]
+                        if data == 'bot':
+                            await self.action_bot()
+                    else:
+                        screen_id = screen['next_screen_id']
+                    if screen['label'] != '':
+                        if screen['is_array'] == 0:
+                            self.response[screen['label']] = data
+                        else:
+                            if screen['label'] not in self.response:
+                                self.response[screen['label']] = []
+                            self.response[screen['label']].append(data)
+                if len(self.response) > 0:
+                    response_id = self.db.put_response(self.client['id'], to_json(self.response))
+                    tracking_message = 'Tracking ID: #' + str(response_id) + '\n'
+                    for key in self.response:
+                        tracking_message += '\n' + key + ':'
+                        if isinstance(self.response[key], list):
+                            for i in range(len(self.response[key])):
+                                tracking_message += '\n ' + str(i+1) + '. ' + self.response[key][i]
+                        else:
+                            tracking_message += '\n ' + self.response[key]
+                        tracking_message += '\n'
+                    await self.socket.send(to_json({
+                        'text': tracking_message,
+                        'options': []
+                    }))
+
+    async def action_bot(self):
+        def normalize(text):
+            lemma = nltk.stem.WordNetLemmatizer()
+            return [lemma.lemmatize(token) for token in
+                    nltk.word_tokenize(text.translate(dict((ord(punct), None) for punct in string.punctuation)))]
+
+        sent_tokens = nltk.sent_tokenize(self.client['description'].lower())
+        await self.socket.send(to_json({
+            'text': 'Hi, I am AI Bot and I am here to answer your questions!',
+            'options': []
+        }))
+        while True:
+            data = str(await self.socket.recv()).lower()
+            if data in ['hi', 'hello', 'hey', 'yo']:
+                await self.socket.send(to_json({
+                    'text': 'Hey',
                     'options': []
                 }))
-            sent_tokens.remove(data)
-
-
-async def action_chat(socket, client, question):
-    await socket.send(to_json({
-        'text': 'What is your name?',
-        'options': []
-    }))
-    name = await socket.recv()
-    await socket.send(to_json({
-        'text': 'What is your email address?',
-        'options': []
-    }))
-    email = await socket.recv()
-    if client['id'] not in queue:
-        queue[client['id']] = []
-    queue[client['id']].append({'socket': socket, '_socket': None, 'name': name, 'email': email, 'question': question})
-    index = len(queue[client['id']]) - 1
-    # TODO: push notifications to client customer support team
-    await socket.send(to_json({
-        'text': 'Waiting for response from customer support team...',
-        'options': ['Cancel']
-    }))
-    while True:
-        data = await socket.recv()
-        if queue[client['id']][index]['_socket'] is None:
-            if data == 'Cancel':
+            elif data in ['ok', 'okay', 'oh', 'k', 'kk', 'no problem', 'can', 'i see', 'lol', 'haha', 'hahaha', 'ya',
+                          'yeah']:
+                await self.socket.send(to_json({
+                    'text': 'Yeah',
+                    'options': []
+                }))
+            elif data in ['thanks', 'thank you']:
+                await self.socket.send(to_json({
+                    'text': 'You are welcome',
+                    'options': []
+                }))
+                break
+            elif data in ['bye']:
+                await self.socket.send(to_json({
+                    'text': 'Goodbye!',
+                    'options': []
+                }))
                 break
             else:
-                await socket.send(to_json({
-                    'text': 'Waiting for response from customer support team...',
-                    'options': ['Cancel']
-                }))
-        else:
-            if queue[client['id']][index]['_socket'].closed:
-                break
-            await queue[client['id']][index]['_socket'].send(data)
+                sent_tokens.append(data)
+                vector = TfidfVectorizer(stop_words='english', tokenizer=normalize).fit_transform(sent_tokens)
+                values = cosine_similarity(vector[-1], vector)
+                idx = values.argsort()[0][-2]
+                flat = values.flatten()
+                flat.sort()
+                if flat[-2] == 0:
+                    await self.socket.send(to_json({
+                        'text': 'I am not sure with that',
+                        'options': []
+                    }))
+                    await self.socket.send(to_json({
+                        'text': 'Do you want to contact customer support?',
+                        'options': ['Yes', 'No']
+                    }))
+                    if await self.socket.recv() == 'Yes':
+                        await self.action_chat(data)
+                        break
+                    else:
+                        await self.socket.send(to_json({
+                            'text': 'I am smarter when you ask me questions with simple keywords :)',
+                            'options': []
+                        }))
+                else:
+                    await self.socket.send(to_json({
+                        'text': sent_tokens[idx],
+                        'options': []
+                    }))
+                sent_tokens.remove(data)
+
+    async def action_chat(self, message):
+        await self.socket.send(to_json({
+            'text': 'What is your name?',
+            'options': []
+        }))
+        name = await self.socket.recv()
+        await self.socket.send(to_json({
+            'text': 'What is your email address?',
+            'options': []
+        }))
+        email = await self.socket.recv()
+        if self.client['id'] not in queue:
+            queue[self.client['id']] = {}
+        chat_id = uuid.uuid4().hex
+        queue[self.client['id']][chat_id] = {'socket': self.socket, '_socket': None, 'messages': [message]}
+        await self.socket.send(to_json({
+            'text': 'Waiting for response from customer support team...',
+            'options': ['Cancel']
+        }))
+        for fcm_token in self.db.get_fcm_tokens(self.client['id']):
+            requests.post('https://fcm.googleapis.com/fcm/send',
+                          headers={'Authorization': 'key=' + config.FCM_API_KEY},
+                          json={
+                              'to': fcm_token,
+                              'priority': 'high',
+                              'notification': {
+                                  'title': name,
+                                  'body': message
+                              },
+                              'data': {
+                                  'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+                                  'id': chat_id,
+                                  'name': name,
+                                  'email': email,
+                                  'message': message
+                              },
+                          })
+        while True:
+            data = await self.socket.recv()
+            if queue[self.client['id']][chat_id]['_socket'] is None:
+                if data == 'Cancel':
+                    break
+                else:
+                    await self.socket.send(to_json({
+                        'text': 'Waiting for response from customer support team...',
+                        'options': ['Cancel']
+                    }))
+            else:
+                if queue[self.client['id']][chat_id]['_socket'].closed:
+                    break
+                await queue[self.client['id']][chat_id]['_socket'].send(await self.socket.recv())
 
 
 def to_json(data): return str(json.dumps(data, ensure_ascii=False))
 
 
-queue = {}
-start_server = websockets.serve(handle_connection, port=8844)
-
-asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_until_complete(websockets.serve(Handler.handle, port=8844))
 asyncio.get_event_loop().run_forever()
-
